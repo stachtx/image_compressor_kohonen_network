081bc807947961be38540b86b9318a4c38abba4b
Tomasz Stachura <203996@edu.p.lodz.pl>
2020-04-28 00:11:34 +0200

    Working compressor

--- Compressor.py
+++ Compressor.py
@@ -2,9 +2,8 @@ from math import log, pow
 
 import numpy as np
 import scipy.misc
-import scipy.misc
 from scipy.cluster.vq import vq
-
+import imageio
 from Config import Config
 from ImageHandler import ImageHandler
 from KohonnenNetwork import KohonnenNetwork
@@ -13,8 +12,9 @@ from KohonnenNetwork import KohonnenNetwork
 class Compressor:
 
     def __init__(self):
-        self.config = Config().compressorConfig
-        self.codebook_size = pow(2, self.config.bits_per_codevector)
+        self.compressorConfig = Config().compressorConfig
+        self.imageHandlerConfig = Config().imageHandlerConfig
+        self.codebook_size = pow(2, self.compressorConfig.bits_per_codevector)
 
     def compress(self):
         kn_rows = int(pow(2, int((log(self.codebook_size, 2)) / 2)))
@@ -22,8 +22,8 @@ class Compressor:
         image_handler = ImageHandler()
         image_vectors = image_handler.split_image_to_blocks()
         number_of_image_vectors = image_vectors.shape[0]
-        kn = KohonnenNetwork(kn_rows, kn_columns, image_handler.vector_dimension, self.config.epochs,
-                             number_of_image_vectors, self.config.initial_learning_rate,
+        kn = KohonnenNetwork(kn_rows, kn_columns, image_handler.vector_dimension, self.compressorConfig.epochs,
+                             number_of_image_vectors, self.compressorConfig.initial_learning_rate,
                              max(kn_rows, kn_columns) / 2)
         reconstruction_values = kn.train(image_vectors)
         image_vector_indices, distance = vq(image_vectors, reconstruction_values)
@@ -31,16 +31,16 @@ class Compressor:
         image_after_compression = np.zeros([image_handler.image_width, image_handler.image_height], "uint8")
         for index, image_vector in enumerate(image_vectors):
             start_row = int(
-                index / (image_handler.image_width / self.config.block_width)) * self.config.block_height
-            end_row = start_row + self.config.block_height
-            start_column = (index * self.config.block_width) % image_handler.image_width
-            end_column = start_column + self.config.block_width
+                index / (image_handler.image_width / self.imageHandlerConfig.block_width)) * self.imageHandlerConfig.block_height
+            end_row = start_row + self.imageHandlerConfig.block_height
+            start_column = (index * self.imageHandlerConfig.block_width) % image_handler.image_width
+            end_column = start_column + self.imageHandlerConfig.block_width
             image_after_compression[start_row:end_row, start_column:end_column] = \
                 np.reshape(reconstruction_values[image_vector_indices[index]],
-                           (self.config.block_width, self.config.block_height))
+                           (self.imageHandlerConfig.block_width, self.imageHandlerConfig.block_height))
 
         output_image_name = "CB_size=" + str(self.codebook_size) + ".png"
-        scipy.misc.imsave(output_image_name, image_after_compression)
+        imageio.imwrite(output_image_name, image_after_compression)
 
         print("Mean Square Error = ", image_handler.mse(image_after_compression))
 
